b8c8ff59b2f79e9be689d3074cceef2400920b81
0joclever <garbobertrand@gmail.com>
2017-04-17 15:33:49 +0200

    Created by Coding Ground Client

---
+++ New Project-20170417/main.rb
@@ -0,0 +1,22 @@
+puts "\e[H\e[2J"
+# Hello World Program in Ruby
+puts "Hello World!";
+
+class Sample
+   def hello
+      puts "Hello Ruby, it'my first try!"
+   end
+end
+
+# Now using above class to create objects
+object1 = Sample. new
+object1.hello
+def hello1
+    puts"The weather is fine today!"
+end
+hello1
+puts "Multiplication Value : #{24*60*6.0571}";
+list = [  "freddo", 10, 3.14, "This is a string", "last element",1222147 ]
+list.each do |i|
+   puts i
+end
